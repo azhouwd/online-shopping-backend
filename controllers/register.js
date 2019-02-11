c6db29a9de6d99656858d00728a985d42573be68
ang <azusmd66@gmail.com>
2019-02-11 12:01:28 -0500

    Revert "good"!
    This reverts commit 7a00e9edc7e304d00b324cb46f5740bfa8e92019.

--- controllers/register.js
+++ controllers/register.js
@@ -1,5 +1,4 @@
-const REDIS_URL = 'redis://h:pf37a69565999be1543016a8a5bec744f7c8fe8313456fae39c4a41d543c76261@ec2-18-215-139-67.compute-1.amazonaws.com:32199'
-const redisClient = redis.createClient(process.env.REDIS_URL);
+const redisClient = require('./signin').redisClient;
 const jwt = require('jsonwebtoken');
 
 const handleRegister = (db,bcrypt,req,res) => {
